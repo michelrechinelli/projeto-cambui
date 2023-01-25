37a9510dc2b18007148b133ab869748d9a0ef243
Michel Rechinelli <michel.tupi2@gmail.com>
2023-01-25 15:41:02 -0300

    criaçãodo menu e iniciação com java script

---
+++ arquivos/SCRIPT/script.js
@@ -0,0 +1,9 @@
+var header = document.getElementById('header');
+var navgation = document.getElementById('navigation');
+var content = document.getElementById('content');
+var showsidebar = false;
+
+function menu(){
+    showsidebar = !showsidebar;
+    console.log()
+}
