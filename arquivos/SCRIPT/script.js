87d99a48d90a6b5a9aa52d6e7fecd08e92c92681
Michel Rechinelli <michel.tupi2@gmail.com>
2023-01-26 22:43:48 -0300

    criação do slider

--- arquivos/SCRIPT/script.js
+++
@@ -1,9 +0,0 @@
-var header = document.getElementById('header');
-var navgation = document.getElementById('navigation');
-var content = document.getElementById('content');
-var showsidebar = false;
-
-function menu(){
-    showsidebar = !showsidebar;
-    console.log()
-}
